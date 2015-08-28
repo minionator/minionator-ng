6ee6a34a3c720cefecdce1686a8c9c7183db7ae6
Rob Thijssen <rthijssen@gmail.com>
2015-08-28 11:11:02 +0100

    yeoman scaffold

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name minionatorNgApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the minionatorNgApp
+ */
+angular.module('minionatorNgApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
